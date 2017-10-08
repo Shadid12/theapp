8f734b61c1af88323d6cab301a3bc50d0ecf1fe5
shadid12 <shadidhaque2012@gmail.com>
2017-10-08 17:05:01 -0400

    looks right so far

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
 
-render() {
-	return(
-		<h1>Well Looks like I am the Header</h1>
-	)
-}
+
+	renderContent() {
+		switch (this.props.auth) {
+		  case null:
+		    return 'Still deciding';
+		  case false:
+		    return 'im loggedout';
+		  default:
+		    return `hello ${this.props.auth.name}`;
+		}
+	}
+
+	render() {
+		return(
+			<div>
+				{this.renderContent()}
+			</div>
+		)
+	}
 
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
