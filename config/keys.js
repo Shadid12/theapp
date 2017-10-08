c8142dc4a958e8255e682815dcb155ea7507da2f
shadid12 <shadidhaque2012@gmail.com>
2017-10-08 02:19:40 -0400

    initial is done

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'googleClientID': '273145355531-nenoe2jhntg9mc195n117o7iq8g7ane7.apps.googleusercontent.com',
+    'googleClientSecret': 'FurKP_ou-GBdYxVG2Lv3isAh',
+    'mongoURI': 'mongodb://root:toor@ds149268.mlab.com:49268/shadid',
+    'cookieKey': 'asdasdllkdooajsdoj'
+}
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+require('./services/passport');
+
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app);
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
