30775b999dec32865df68cc9ad03bb7b46f8be29
shadid12 <shadidhaque2012@gmail.com>
2017-10-08 15:02:34 -0400

    ready to patch up the states

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+render() {
+	return(
+		<h1>Well Looks like I am the Header</h1>
+	)
+}
+
+}
+
+export default Header;
